e1a46e6504a149e60203e667602c1ff093287a2b
thetuxwhocodes <invincibletux@gmail.com>
2014-07-28 17:02:20 +0530

    Initial version

---
+++ src/com/thetuxwhocodes/generic/PrintSystemProperties.java
@@ -0,0 +1,27 @@
+package com.thetuxwhocodes.generic;
+
+import java.util.Properties;
+import java.util.Set;
+import java.util.Map.Entry;
+
+public class PrintSystemProperties {
+
+	public static void printAllSystemProperties() {
+
+		Properties systemProperties = System.getProperties();
+
+		Set<Entry<Object, Object>> propertyEntries = systemProperties
+				.entrySet();
+
+		for (Entry<Object, Object> propertyEntry : propertyEntries) {
+			System.out.println(propertyEntry.getKey() + " : "
+					+ propertyEntry.getValue());
+		}
+
+	}
+
+	public static void main(String[] args) {
+		printAllSystemProperties();
+	}
+
+}
